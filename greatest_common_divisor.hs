0176158f416cffc279872b9bb0f693409b89ca15
hckr <hckr@eml.cc>
2014-09-13 14:10:15 -0500

    Make sure to take the absolute value.

--- greatest_common_divisor.hs
+++ greatest_common_divisor.hs
@@ -1,11 +1,12 @@
---- Uses the Euclidean algorithm to compute the greatest common divisor.
+--- Euclidean Divisor Algorithm
+--- This is a fast way to compute the greatest common divisor of two integers.
 greatestCommonDivisor :: Int -> Int -> Int
 greatestCommonDivisor x y = if (remainder a b) == 0
                               then b
                               else greatestCommonDivisor a (remainder a b)
                             where
-                              a = (max x y)
-                              b = (min x y)
+                              a = absoluteValue (max x y)
+                              b = absoluteValue (min x y)
                             
 absoluteValue :: Int -> Int
 absoluteValue n = if n > 0 then n else (n)*(-1)
