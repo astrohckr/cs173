24e218995dfc4818cf27a39d1430aeace6675bbb
hckr <hckr@eml.cc>
2014-09-13 16:24:24 -0500

    Some more progress.

--- primes_calculator.hs
+++ primes_calculator.hs
@@ -1,7 +1,10 @@
 -- Eratosthenes Sieve implementation in Haskell. 
 -- Find the primes
 -- When n = 10, primes(10) = [2,3,5,7]
-primes n = [(square x) + x|x <- [2..n], (square x) < n]
+-- [2,3,4,5,6,7,8,9,10]
+-- [2,3]
+-- [2,3,5,7]
+sieve n = [x|x <- [2..n], (square x) < n]
 
 square :: Int -> Int
 square x = (*) x x
