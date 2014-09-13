549aa9926d1d7ffb438e71262c4af546ef1d0767
hckr <hckr@eml.cc>
2014-09-13 13:40:18 -0500

    Break into lines.

--- gcd.hs
+++ gcd.hs
@@ -1,3 +1,5 @@
 greatestCommonDivisor :: Int -> Int -> Int
-greatestCommonDivisor x y = if (x `mod` y) == 0 then y else greatestCommonDivisor x (x `mod` y)
+greatestCommonDivisor x y = if (x `mod` y) == 0 
+                              then y 
+                              else greatestCommonDivisor x (x `mod` y)
                             
