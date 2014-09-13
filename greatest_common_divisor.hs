889575ca530a8926d367ba7b99ffcddd070ace49
hckr <hckr@eml.cc>
2014-09-13 13:49:05 -0500

    Account for when x < y in input.

--- greatest_common_divisor.hs
+++ greatest_common_divisor.hs
@@ -1,6 +1,6 @@
 --- Uses the Euclidean algorithm to compute the greatest common divisor.
 greatestCommonDivisor :: Int -> Int -> Int
-greatestCommonDivisor x y = if (x `mod` y) == 0 
-                              then y 
-                              else greatestCommonDivisor x (x `mod` y)
+greatestCommonDivisor x y = if ((max x y) `mod` (min x y)) == 0 
+                              then (min x y)
+                              else greatestCommonDivisor (max x y) ((max x y) `mod` (min x y))
                             
