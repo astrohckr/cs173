767a025be10a7231c4c9757b38466535e5c50a63
hckr <hckr@eml.cc>
2014-09-13 14:07:45 -0500

    Use function for remainder.

--- greatest_common_divisor.hs
+++ greatest_common_divisor.hs
@@ -1,8 +1,8 @@
 --- Uses the Euclidean algorithm to compute the greatest common divisor.
 greatestCommonDivisor :: Int -> Int -> Int
-greatestCommonDivisor x y = if (a `mod` b) == 0
+greatestCommonDivisor x y = if (remainder a b) == 0
                               then b
-                              else greatestCommonDivisor a (a `mod` b)
+                              else greatestCommonDivisor a (remainder a b)
                             where
                               a = (max x y)
                               b = (min x y)
@@ -11,3 +11,6 @@ absoluteValue :: Int -> Int
 absoluteValue n = if n > 0
                     then n
                     else (n)*(-1)
+
+remainder :: Int -> Int -> Int
+remainder a b = a `mod` b
