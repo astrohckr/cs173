0c97a2ca60c5e0ad9f6d5183c28f02df16e42fed
hckr <hckr@eml.cc>
2014-09-13 13:45:34 -0500

    Renamed file.
    Signed-off-by: hckr <hckr@eml.cc>

--- greatest_common_divisor.hs
+++ greatest_common_divisor.hs
@@ -1,3 +1,4 @@
+--- Uses the Euclidean algorithm to compute the greatest common divisor.
 greatestCommonDivisor :: Int -> Int -> Int
 greatestCommonDivisor x y = if (x `mod` y) == 0 
                               then y 
